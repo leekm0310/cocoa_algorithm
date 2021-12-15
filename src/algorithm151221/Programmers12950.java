d4f47a501840d6c09feda275232708f2d75bf973
Kyungmi Lee <leekm310@gmail.com>
2021-12-15 22:36:56 +0900

    프로그래머스 12950 완료

---
+++ src/algorithm151221/Programmers12950.java
@@ -0,0 +1,20 @@
+package algorithm151221;
+
+import java.util.Arrays;
+
+public class Programmers12950 {
+    public static void main(String[] args) {
+        int[][] answer ={};
+        int[][] arr1 = {{1,2},{2,3}};
+        int[][] arr2 = {{3,4},{5,6}};
+
+        answer = new int[arr1.length][arr1[1].length];
+        for (int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr1[i].length; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        System.out.println(Arrays.deepToString(answer));
+
+    }
+}
